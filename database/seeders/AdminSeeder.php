a2c131b4b4a9d624770e89186c9cdcf291d5b088
ricardo.ralds <ricardo.rals.dev@gmail.com>
2022-09-05 08:10:33 -0300

    Instalando breeze e spatie, criando seeder de roles e usuario,adicionando side bar e criando index para as roles

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+   
+    public function run()
+    { 
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(123456789), // password,
+        ])->assignRole('admin');
+    }
+}
